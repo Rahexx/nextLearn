f2e97870d1105b4d590fca4a1e3d3489ef6ce90a
Andrzej Łuszcz <endriu1297@gmail.com>
2023-08-29 21:28:06 +0200

    learn optional catch all in routing

---
+++ src/app/optionalCatchAll/[[...slug]]/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+export default function CatchAll({ params }: { params: { slug: string } }) {
+  const router = useRouter();
+
+  return (
+    <>
+      <p>Jesteś w segmencie optionalCatchAll {params.slug}</p>
+      <p>
+        Ścieżka <b>/optionalCatchAll/{params.slug}</b>
+      </p>
+      <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        onClick={() => router.push('/')}
+      >
+        Na Strone Główną
+      </button>
+    </>
+  );
+}
